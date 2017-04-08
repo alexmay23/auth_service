4abd204c5c2b10976c57b6dd772070f4a30981f7
Alex Moiseenko <alexmoiseenko@me.com>
2017-04-08 05:48:41 +0300

    Auroreconnect. Appended;

--- service/db.py
+++ service/db.py
@@ -1,7 +1,7 @@
 import os
 import uuid
 from binascii import hexlify
-from asynclib.database.mongo import MongoDBManager
+from asynclib.database.mongo import MongoDBManager, auto_reconnect
 from asynclib.utils.functions import filter_only_keys
 
 from service.instances import service
@@ -34,6 +34,7 @@ class UserDBManager(MongoDBManager):
         return await self.update_one_denormalized({'_id': _id}, {'$set': filter_only_keys(kwargs,
                                                                                           ['timezone', 'locale'])})
 
+    @auto_reconnect
     async def generate_id(self):
         cursor = self.db.find({}).skip(0).limit(1).sort([('uid', - 1)])
         if await cursor.count() == 0:
@@ -41,6 +42,7 @@ class UserDBManager(MongoDBManager):
         result = await cursor.to_list(1)
         return result[0].get('uid') + 1
 
+    @auto_reconnect
     async def append_identity(self, user_id, identity_type, value):
         result = await self.db.update_one({'_id': user_id}, {
             'type': identity_type,
@@ -50,18 +52,22 @@ class UserDBManager(MongoDBManager):
         })
         return result
 
+    @auto_reconnect
     async def set_password(self, _id, password):
         await self.db.update_one({'_id': _id}, {'$set': {'password': password_hash(password)}})
 
+    @auto_reconnect
     async def create_verification_context(self, uid, verification_context):
         await self.db.update_one({'identities.uid': uid}, {'$set':
                                                                {
                                                                    'identities.$.verification_context': verification_context}})
 
+    @auto_reconnect
     async def verify(self, uid):
         await self.db.update_one({'identities.uid': uid}, {'$set': {'identities.$.verified': True},
                                                            '$unset': {'identities.$.verification_context': 1}})
 
+    @auto_reconnect
     async def get_by_identity(self, identity):
         result = await self.db.find_one({'identities.value': identity})
         return result
